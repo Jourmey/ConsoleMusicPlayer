a7a1a44fce297328ff42ddf6152243721910c670
郑佳明 <zhengjm@novastar.tech>
2020-06-01 20:01:09 +0800

    firstcommit

---
+++ ConsoleMusicPlayer/Audio/AudioPlayback.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NAudio.Wave;
+
+
+namespace ConsoleMusicPlayer.Audio
+{
+
+    public class AudioPlayback : IDisposable
+    {
+        private IWavePlayer playbackDevice;
+        private WaveStream fileStream;
+
+
+        public AudioPlayback(/*IApplicationShell appShell*/)
+        {
+            this.playbackDevice = new WaveOut { DesiredLatency = 200 };
+        }
+
+        private void CloseFile()
+        {
+            if (fileStream != null)
+            {
+                fileStream.Dispose();
+                fileStream = null;
+            }
+        }
+
+        public void OpenFile(AudioFileReader fileReader, SampleAggregator aggregator)
+        {
+            try
+            {
+                this.fileStream = fileReader;
+                playbackDevice.Init(aggregator);
+
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show(e.Message, "Problem opening file");
+                CloseFile();
+            }
+        }
+
+
+        public void Play()
+        {
+            if (playbackDevice != null && fileStream != null && playbackDevice.PlaybackState != PlaybackState.Playing)
+            {
+                playbackDevice.Play();
+            }
+        }
+
+        public void Pause()
+        {
+            if (playbackDevice != null)
+            {
+                playbackDevice.Pause();
+            }
+        }
+
+        public void Stop()
+        {
+            if (playbackDevice != null)
+            {
+                playbackDevice.Stop();
+            }
+            if (fileStream != null)
+            {
+                fileStream.Position = 0;
+            }
+        }
+
+        public void Dispose()
+        {
+            Stop();
+            CloseFile();
+            if (playbackDevice != null)
+            {
+                playbackDevice.Dispose();
+                playbackDevice = null;
+            }
+        }
+    }
+}
---
+++ ConsoleMusicPlayer/Program.cs
@@ -0,0 +1,62 @@
+﻿using ConsoleMusicPlayer;
+using ConsoleMusicPlayer.Audio;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleMusicPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string file = @"C:\Users\Administrator\Desktop\庆庆 - 蝙蝠.mp3";
+            var inputStream = new AudioFileReader(file);
+
+            var aggregator = new SampleAggregator(inputStream);
+            aggregator.NotificationCount = inputStream.WaveFormat.SampleRate / 100;
+            aggregator.PerformFFT = true;
+
+            AudioPlayback audioPlayback = new AudioPlayback();
+            audioPlayback.OpenFile(inputStream, aggregator);
+            audioPlayback.Play();
+
+
+            var a = new AudioAnalyzer(aggregator);
+
+            a.AnalyzedAudioEventHandler += A_AnalyzedAudioEventHandler;
+
+            var s = Console.ReadKey();
+
+        }
+
+        private static void A_AnalyzedAudioEventHandler(object sender, AnalyzedAudioEventArgs e)
+        {
+
+            if (e?.AnalyzedAudio.Samples != null)
+            {
+                var max = e.AnalyzedAudio.Samples.Sum((w) => w.MaxSample);
+                var min = e.AnalyzedAudio.Samples.Sum((w) => w.MinSample);
+
+                StringBuilder a = new StringBuilder();
+
+
+                for (int i = 0; i < 0 - (int)(min * 10); i++)
+                {
+                    a.Append("+");
+                }
+
+                for (int i = 0; i < (int)(max * 10); i++)
+                {
+                    a.Append("-");
+                }
+
+
+                Console.WriteLine(a.ToString());
+            }
+        }
+    }
+}
