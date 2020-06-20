a5480ac460e3f54efeda6d5456f130906e1adba5
Jourmey <284655785@qq.com>
2020-06-20 19:41:28 +0800

    Update Program.cs

--- ConsoleMusicPlayer/Program.cs
+++ ConsoleMusicPlayer/Program.cs
@@ -4,25 +4,62 @@ using NAudio.Wave;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 
 namespace ConsoleMusicPlayer
 {
     class Program
     {
+        private static MusicService _musicService;
+
         static void Main(string[] args)
         {
-            MusicService musicService = new MusicService();
-            musicService.MusicWaveEventHandler += MusicService_MusicWaveEventHandler;
-            musicService.MusicContextEventHandler += MusicService_MusicContextEventHandler;
+            _musicService = new MusicService();
+            _musicService.MusicWaveEventHandler += MusicService_MusicWaveEventHandler;
+            _musicService.SongContextEventHandler += MusicService_MusicContextEventHandler;
 
-            musicService.Play();
+            _musicService.Init();
+            _musicService.Play();
+
+            Timer timer = new Timer(updateUIWave, null, 500, 100);
+
+
             var s = Console.ReadKey();
+            while (true)
+            {
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        if (_musicService.IsPlaying())
+                        {
+                            _musicService.Pause();
+                        }
+                        else
+                        {
+                            _musicService.Play();
+                        }
+                        break;
+                    case ConsoleKey.N:
+                        _musicService.Next();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
 
         }
 
+        private static void updateUIWave(object state)
+        {
 
-        private const int MaxHeight = 10;
+            MusicService_MusicWaveEventHandler(null, _musicService.GetMusicWave());
+        }
+
+        private const int MaxHeight = 12;
         private const int MaxWidth = 32;
         private static int[] _topPosition = new int[MaxWidth];
         private static bool[,] _topP = new bool[MaxWidth, MaxHeight];
@@ -36,13 +73,13 @@ namespace ConsoleMusicPlayer
             }
 
 
-            for (int i = 0; i < MaxWidth; i++)
+            for (int i = 0; i < Math.Min(MaxWidth, e.Wave.Length); i++)
             {
                 var w = e.Wave[i] * MaxHeight / 256;
 
                 for (int j = 0; j < MaxHeight; j++)
                 {
-                    if (j <= w)
+                    if (j <= (MaxHeight - w))
                     {
                         _topP[i, j] = false;
                     }
@@ -57,15 +94,15 @@ namespace ConsoleMusicPlayer
 
             }
 
-            // 降落滑块
-            for (int i = 0; i < MaxWidth; i++)
-            {
-                var j = MaxHeight - _topPosition[i];
-                if (j >= 0 && j < MaxHeight)
-                {
-                    _topP[i, j] = true;
-                }
-            }
+            //// 降落滑块
+            //for (int i = 0; i < MaxWidth; i++)
+            //{
+            //    var j = MaxHeight - _topPosition[i];
+            //    if (j >= 0 && j < MaxHeight)
+            //    {
+            //        _topP[i, j] = true;
+            //    }
+            //}
 
 
             for (int j = 0; j < MaxHeight; j++)
@@ -87,9 +124,31 @@ namespace ConsoleMusicPlayer
                 Console.WriteLine(buffer2.ToArray(), 0, buffer2.Count);
             }
 
+            Console.SetCursorPosition(5, 20);
+            Console.WriteLine(getSongProcess(e.IsPlaying, e.CurrentTime, e.TotalTime));
+
         }
 
-        private static void MusicService_MusicContextEventHandler(object sender, MusicContext e)
+        private static string getSongProcess(bool isPlaying, TimeSpan currentTime, TimeSpan totalTime)
+        {
+            var thu = (int)(currentTime.TotalSeconds / totalTime.TotalSeconds * 20) - 1;
+
+            StringBuilder s = new StringBuilder("--------------------");
+
+
+            if (thu >= 0 && thu < s.Length)
+            {
+                s[thu] = '◆';
+            }
+
+
+            string p = isPlaying ? "▲" : "■";
+
+            return $"{p}  {currentTime.ToString(@"mm\:ss")} {s} {totalTime.ToString(@"mm\:ss")}";
+
+        }
+
+        private static void MusicService_MusicContextEventHandler(object sender, SongContext e)
         {
             if (e == null)
             {
@@ -100,7 +159,6 @@ namespace ConsoleMusicPlayer
             Console.ForegroundColor = ConsoleColor.Cyan;
 
             Console.WriteLine("---------------------");
-            Console.WriteLine($"Last |{e.LastName}");
             Console.WriteLine($"Next |{ e.NextName }");
             Console.WriteLine($"Now  |{ e.MusicName}");
             Console.WriteLine("---------------------");
